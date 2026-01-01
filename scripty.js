4a044aa57ea92d05d1d5ec4b7dbd833bbbf2aae7
BountyNinjaHunter <252030807+bountyninjahunter@users.noreply.github.com>
2026-01-01 06:49:46 -0500

    Updated files and code. Dynamic site in progress!

--- scripty.js
+++ scripty.js
@@ -1,7 +1,7 @@
 let myDate = new Date;
 console.log(myDate);
 
-function myAge(n1, n2){
+/* function myAge(n1, n2){
     return n1 - n2;
 }
 
@@ -34,3 +34,58 @@ let text2 = text1.replaceAll("world", "den");
 console.log(text2);
 
 console.log(document.querySelector(".top"))
+*/ 
+
+
+//---------------------------------------------------
+
+let para = document.getElementById("para");
+para.textContent="Changed paragraphs";
+para.style.color = "green"
+
+let img = document.querySelector(".imgy");
+//img.setAttribute("src","./Images/hacker.png");
+
+let a = document.querySelector(".linky");
+a.setAttribute("href", "https://www.youtube.com");
+a.innerHTML="<span>Youtube Link</span>";
+
+const divs = document.querySelectorAll("div");
+console.log(divs);
+
+const list = document.querySelectorAll("li");
+
+//for (i = 0; 1 < list.length; i++){
+//    list[i].style.color = "purple";
+//}
+
+
+const ul = document.querySelector("ul");
+const li1 = document.createElement("li");
+
+ul.append(li1);
+
+li1.innerText = "Buttercup";
+li1.setAttribute("class", "top");
+li1.removeAttribute("class"); 
+
+ul.childNodes[1].style.color="green";
+
+li1.previousElementSibling.style.color = "green";
+//li1.remove();
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+    alert("You have been sent to Saturn!")
+})
+
+const imgID = document.querySelector("#imgID");
+
+imgID.addEventListener("mouseover", () => {
+
+    img.setAttribute("src","./Images/hacker.png");
+})
+
+imgID.addEventListener("mouseout", () => {
+
+    img.setAttribute("src","./Images/images.png");
+})
